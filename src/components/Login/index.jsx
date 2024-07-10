6731134ce7f5b2e469009d87e56d6a538767370a
Kumar Satyam <142577338+kumarsatyam-ksa@users.noreply.github.com>
2024-07-10 14:15:52 +0530

    Update index.jsx

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:3000/api/auth";
+      const url = "https://petify-backend.onrender.com/api/auth";
       const { data: res } = await axios.post(url, data);
       localStorage.setItem("token", res.token);
       navigate("/home");
